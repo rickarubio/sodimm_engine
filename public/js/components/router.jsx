125adbddd4d0bf8b65772bcceae649248b0b16e0
Harry Moreno <morenoh149@gmail.com>
2015-02-07 22:41:14 -0800

    add react-router to project

---
+++ public/js/components/router.jsx
@@ -0,0 +1,22 @@
+var Route = ReactRouter.Route;
+var RouteHandler = ReactRouter.Handler;
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <RouteHandler />
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route handler={App} path="/">
+    <Route name="chatroom" handler={Chatroom} />
+    <Route name="chatrooms" handler={Chatrooms} />
+  </Route>
+);
+
+ReactRouter.run(routes, ReactRouter.HistoryLocation, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
